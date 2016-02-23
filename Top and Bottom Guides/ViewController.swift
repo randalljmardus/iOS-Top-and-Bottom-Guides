31d5883a1a51e16c3768187119b248f7afe90506
Randall Mardus <randalljmardus@gmail.com>
2016-02-23 13:41:11 -0500

    first commit

--- Top and Bottom Guides/ViewController.swift
+++ Top and Bottom Guides/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+    let box = UIView()
+        box.backgroundColor = UIColor.redColor()
+        box.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(box)
+        
+        box.widthAnchor.constraintEqualToConstant(200).active = true
+        box.heightAnchor.constraintEqualToConstant(100).active = true
+        
+        
+    
+    
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
